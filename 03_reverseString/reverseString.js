ad578f732d6283959b9a88037350a839e523cb20
Aryaman <aryaman250100@gmail.com>
2023-10-05 23:21:07 +0530

    Solve exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,5 @@
-const reverseString = function() {
-
+const reverseString = function(phrase) {
+    return phrase.split("").reverse().join("");
 };
-
 // Do not edit below this line
 module.exports = reverseString;
