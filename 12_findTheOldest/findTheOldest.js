1a1b434de5fd69bd4c69105f891b276f178ec11f
Aryaman <aryaman250100@gmail.com>
2023-10-10 15:38:53 +0530

    Solve exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(persons) {
+    return persons.reduce((oldest, person) => {
+        if(oldest.name === undefined) return person;
+        if(oldest.yearOfDeath === undefined) oldest.yearOfDeath = new Date().getFullYear(); 
+        const age = person.yearOfDeath - person.yearOfBirth;
+        const oldestAge = oldest.yearOfDeath - oldest.yearOfBirth;
+        return age > oldestAge ? person : oldest;
+    }, {})
 };
 
 // Do not edit below this line
