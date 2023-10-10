0079777558322f691500f054fa9486c0b57bf993
Aryaman <aryaman250100@gmail.com>
2023-10-10 14:39:46 +0530

    Solve exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if(num < 0) return 'OOPS';
+    if(num <= 2) return 1;
+    let prev = 1;
+    let prevprev = 1;
+    let curr;
+    for(let i = 3; i <= num; i++) {
+        curr = prev+prevprev;
+        prevprev = prev;
+        prev = curr;
+    }
+    return curr;
 };
 
 // Do not edit below this line
