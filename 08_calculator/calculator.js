764bbb582aa82b5f70ccaf4ae0302213dcef5e02
Aryaman <aryaman250100@gmail.com>
2023-10-10 13:58:43 +0530

    Solve exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return arr.reduce((total, a) => total + a, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((product, a) => product * a, 1);
 };
 
-const power = function() {
-	
+const power = function(a, exp) {
+  return a ** exp;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let res = 1;
+  for(let i=a; i>1; i--) {
+    res *= i;
+  }
+  return res;
 };
 
 // Do not edit below this line
